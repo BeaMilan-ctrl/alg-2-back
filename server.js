3f991164af29345e3278c3d86aa5f05f686794fe
Beatriz Milan <bia.milan@icloud.com>
2025-03-27 14:55:02 -0400

    Include script build

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(cors());
 
 const prisma = new PrismaClient();
 
-const port = 3000;
+ const port = process.env.PORT || 3000;
+
+ app.get("/", (req, res) => {
+  res.send("API rodando!");
+});
 
 app.get("/notas", async (req, res) => {
   let notas = [];
@@ -51,7 +55,7 @@ app.put("/notas/:id", async (req, res) => {
       deadline: req.body.deadline,
     },
     where: {
-      id
+      id : Number (id)
     }
   });
 
@@ -63,7 +67,7 @@ app.delete("/notas/:id", async (req, res) => {
 
   const deleted_nota = await prisma.notas.delete({
     where: {
-      id
+      id : Number (id)
     }
   });
 
